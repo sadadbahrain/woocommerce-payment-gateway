0e54c0c64464be839273f16fbbe205073bd4c3e0
SADAD Electronic Payment System BSC Closed <subscriptions@sadadbahrain.com>
2020-05-27 17:52:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,140 +9,151 @@
  *
  * License: GNU General Public License v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- */
- 
- 
-add_filter( 'woocommerce_payment_gateways', 'sadad_add_gateway_class' );
-function sadad_add_gateway_class( $gateways ) {
-	$gateways[] = 'WC_SADAD_Gateway'; // your class name is here
-	return $gateways;
+*/
+
+add_filter('woocommerce_payment_gateways', 'sadad_add_gateway_class');
+function sadad_add_gateway_class($gateways)
+{
+    $gateways[] = 'WC_SADAD_Gateway'; // your class name is here
+    return $gateways;
 }
- 
-add_action( 'plugins_loaded', 'sadad_init_gateway_class' );
-function sadad_init_gateway_class() {
- 
-	class WC_SADAD_Gateway extends WC_Payment_Gateway {
- 
- 		public function __construct() {
- 
-			$this->id = 'sadded';
-			$this->icon = 'https://www.sadadbahrain.com/img/logo2.png';
-			$this->has_fields = true;
-			$this->method_title = 'SADDED By SADAD Bahrain';
-			$this->method_description = 'SADAD Bahrain payment gateway for Debit and Credit cards.';
-		 
-			$this->supports = array(
-				'products'
-			);
-		 
-			$this->init_form_fields();
-		 
-			$this->init_settings();
-			$this->title = $this->get_option( 'title' );
-			$this->description = $this->get_option( 'description' );
-			$this->enabled = $this->get_option( 'enabled' );
-			$this->testmode = 'yes' === $this->get_option( 'testmode' );
-			$this->private_key = $this->testmode ? $this->get_option( 'test_private_key' ) : $this->get_option( 'private_key' );
-			$this->publishable_key = $this->testmode ? $this->get_option( 'test_publishable_key' ) : $this->get_option( 'publishable_key' );
-		 
-			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-		 
-			add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
-		 
- 		}
- 
- 		public function init_form_fields(){
- 
-		$this->form_fields = array(
-				'enabled' => array(
-				'title'       => 'Enable/Disable',
-				'label'       => 'Enable Gateway',
-				'type'        => 'checkbox',
-				'description' => '',
-				'default'     => 'no'
-				),
-				'title' => array(
-				'title'       => 'Title',
-				'type'        => 'text',
-				'description' => 'This controls the title which the user sees during checkout.',
-				'default'     => 'SADDED By SADAD',
-				'desc_tip'    => true,
-				),
-				'description' => array(
-				'title'       => 'Description',
-				'type'        => 'textarea',
-				'description' => 'This controls the description which the user sees during checkout.',
-				'default'     => 'Pay with your credit card or debit card with SADAD Bahrain.',
-				),
-				'testmode' => array(
-				'title'       => 'Test mode',
-				'label'       => 'Enable Test Mode',
-				'type'        => 'checkbox',
-				'description' => 'Place the payment gateway in test mode using test API keys.',
-				'default'     => 'yes',
-				'desc_tip'    => true,
-				),
-				'api_key' => array(
-				'title'       => 'Api Key',
-				'type'        => 'text'
-				),
-				'vendor_id' => array(
-				'title'       => 'Vendor ID',
-				'type'        => 'text',
-				),
-				'branch_id' => array(
-				'title'       => 'Branch ID',
-				'type'        => 'text',
-				),
-				'terminal_id' => array(
-				'title'       => 'Terminal ID',
-				'type'        => 'text',
-				)
-			);
- 
-	 	}
- 
-		/**
-		 * You will need it if you want your custom credit card form, Step 4 is about it
-		 */
-		public function payment_fields() {
- 
-			
- 
-		}
- 
-		/*
-		 * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
-		 */
-	 	public function payment_scripts() {
- 
-			
- 
-	 	}
- 
-		/*
- 		 * Fields validation, more in Step 5
-		 */
-		public function validate_fields() {
- 
-			if( empty( $_POST[ 'billing_first_name' ]) ) {
-        		wc_add_notice(  'First name is required!', 'error' );
-        		return false;
-        	}
-        	return true;
- 
-		}
- 
-		/*
-		 * We're processing the payments here, everything about it is in Step 5
-		 */
-		public function process_payment( $order_id ) {
- 
-			global $woocommerce;
-	        $date = new DateTime;
-	        $order = wc_get_order( $order_id );
+
+add_action('plugins_loaded', 'sadad_init_gateway_class');
+function sadad_init_gateway_class()
+{
+
+    class WC_SADAD_Gateway extends WC_Payment_Gateway
+    {
+
+        public function __construct()
+        {
+
+            $this->id = 'sadded';
+            $this->icon = 'https://www.sadadbahrain.com/img/logo2.png';
+            $this->has_fields = true;
+            $this->method_title = 'SADDED By SADAD Bahrain';
+            $this->method_description = 'SADAD Bahrain payment gateway for Debit and Credit cards.';
+
+            $this->supports = array(
+                'products'
+            );
+
+            $this->init_form_fields();
+
+            $this->init_settings();
+            $this->title = $this->get_option('title');
+            $this->description = $this->get_option('description');
+            $this->enabled = $this->get_option('enabled');
+            $this->testmode = 'yes' === $this->get_option('testmode');
+            $this->private_key = $this->testmode ? $this->get_option('test_private_key') : $this->get_option('private_key');
+            $this->publishable_key = $this->testmode ? $this->get_option('test_publishable_key') : $this->get_option('publishable_key');
+
+            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array(
+                $this,
+                'process_admin_options'
+            ));
+
+            add_action('wp_enqueue_scripts', array(
+                $this,
+                'payment_scripts'
+            ));
+
+        }
+
+        public function init_form_fields()
+        {
+
+            $this->form_fields = array(
+                'enabled' => array(
+                    'title' => 'Enable/Disable',
+                    'label' => 'Enable Gateway',
+                    'type' => 'checkbox',
+                    'description' => 'Pay with your credit card or debit card with SADAD Bahrain.',
+                    'default' => 'no'
+                ) ,
+                'title' => array(
+                    'title' => 'Title',
+                    'type' => 'text',
+                    'description' => 'This controls the title which the user sees during checkout.',
+                    'default' => 'SADDED By SADAD',
+                    'desc_tip' => true,
+                ) ,
+                'description' => array(
+                    'title' => 'Description',
+                    'type' => 'textarea',
+                    'description' => 'This controls the description which the user sees during checkout.',
+                    'default' => 'Pay with your credit card or debit card with SADAD Bahrain.',
+                ) ,
+                'testmode' => array(
+                    'title' => 'Test mode',
+                    'label' => 'Enable Test Mode',
+                    'type' => 'checkbox',
+                    'description' => 'Place the payment gateway in test mode using test API keys.',
+                    'default' => 'yes',
+                    'desc_tip' => true,
+                ) ,
+                'api_key' => array(
+                    'title' => 'Api Key',
+                    'type' => 'text'
+                ) ,
+                'vendor_id' => array(
+                    'title' => 'Vendor ID',
+                    'type' => 'text',
+                ) ,
+                'branch_id' => array(
+                    'title' => 'Branch ID',
+                    'type' => 'text',
+                ) ,
+                'terminal_id' => array(
+                    'title' => 'Terminal ID',
+                    'type' => 'text',
+                )
+            );
+
+        }
+
+        /**
+         * You will need it if you want your custom credit card form, Step 4 is about it
+         */
+        public function payment_fields()
+        {
+
+        }
+
+        /*
+         * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
+        */
+        public function payment_scripts()
+        {
+
+        }
+
+        /*
+         * Fields validation, more in Step 5
+        */
+        public function validate_fields()
+        {
+
+            if (empty($_POST['billing_first_name']))
+            {
+                wc_add_notice('First name is required!', 'error');
+                return false;
+            }
+            return true;
+
+        }
+
+        /*
+         * We're processing the payments here, everything about it is in Step 5
+        */
+        public function process_payment($order_id)
+        {
+
+            global $woocommerce;
+            $date = new DateTime;
+            $order = wc_get_order($order_id);
             $total = $order->get_total();
-	        $order_data = $order->get_data(); // The Order data
+            $order_data = $order->get_data(); // The Order data
             $order_id = $order_data['id'];
             $order_parent_id = $order_data['parent_id'];
             $order_status = $order_data['status'];
@@ -152,17 +163,14 @@ function sadad_init_gateway_class() {
             $order_payment_method_title = $order_data['payment_method_title'];
             $order_payment_method = $order_data['payment_method'];
             $order_payment_method = $order_data['payment_method'];
-            
-            ## Creation and modified WC_DateTime Object date string ##
-            
-            // Using a formated date ( with php date() function as method)
+
             $order_date_created = $order_data['date_created']->date('Y-m-d H:i:s');
             $order_date_modified = $order_data['date_modified']->date('Y-m-d H:i:s');
-            
+
             // Using a timestamp ( with php getTimestamp() function as method)
             $order_timestamp_created = $order_data['date_created']->getTimestamp();
             $order_timestamp_modified = $order_data['date_modified']->getTimestamp();
-            
+
             $order_discount_total = $order_data['discount_total'];
             $order_discount_tax = $order_data['discount_tax'];
             $order_shipping_total = $order_data['shipping_total'];
@@ -171,11 +179,9 @@ function sadad_init_gateway_class() {
             $order_total = $order_data['total'];
             $order_total_tax = $order_data['total_tax'];
             $order_customer_id = $order_data['customer_id']; // ... and so on
-            
             $order_total = $order_data['total'];
-            
+
             ## BILLING INFORMATION:
-            
             $order_billing_first_name = $order_data['billing']['first_name'];
             $order_billing_last_name = $order_data['billing']['last_name'];
             $order_billing_company = $order_data['billing']['company'];
@@ -187,9 +193,8 @@ function sadad_init_gateway_class() {
             $order_billing_country = $order_data['billing']['country'];
             $order_billing_email = $order_data['billing']['email'];
             $order_billing_phone = $order_data['billing']['phone'];
-            
+
             ## SHIPPING INFORMATION:
-            
             $order_shipping_first_name = $order_data['shipping']['first_name'];
             $order_shipping_last_name = $order_data['shipping']['last_name'];
             $order_shipping_company = $order_data['shipping']['company'];
@@ -199,99 +204,103 @@ function sadad_init_gateway_class() {
             $order_shipping_state = $order_data['shipping']['state'];
             $order_shipping_postcode = $order_data['shipping']['postcode'];
             $order_shipping_country = $order_data['shipping']['country'];
-            
-	        $this->init_form_fields();
-        	$this->init_settings();
-        	$this->title = $this->get_option( 'title' );
-        	$this->description = $this->get_option( 'description' );
-        	$this->enabled = $this->get_option( 'enabled' );
-        	$this->testmode = 'yes' === $this->get_option( 'testmode' );
-        	$this->api_key = $this->get_option( 'api_key' );
-        	$this->vendor_id = $this->get_option( 'vendor_id' );
-        	$this->branch_id = $this->get_option( 'branch_id' );
-        	$this->terminal_id = $this->get_option( 'terminal_id' );
-        	$orderIdString = '?orderId=' . $order_id;
-	        $data = array(
-                'api-key'	=> $this->api_key,
-                'vendor-id'	=> $this->vendor_id,
-                'branch-id'	    => $this->branch_id,
-                'terminal-id'	=> $this->terminal_id,
-                'msisdn'	    => $order_billing_phone,
-                'email'        => $order_billing_email,
-                'customer-name'	=> $order_billing_first_name . " " . $order_billing_last_name,
-                'amount'	=>      strval($total),
-                'description'	=> $order_id,
-                'date'	=> $date->format('Y-m-d H:i:s'),
-                'notification-mode'	=> '200',
-                'success-url'   =>  site_url().'/wc-api/sadad_payment_success/'.$orderIdString,
-                'error-url' =>  site_url().'/wc-api/sadad_payment_failure/'.$orderIdString,
+
+            $this->init_form_fields();
+            $this->init_settings();
+            $this->title = $this->get_option('title');
+            $this->description = $this->get_option('description');
+            $this->enabled = $this->get_option('enabled');
+            $this->testmode = 'yes' === $this->get_option('testmode');
+            $this->api_key = $this->get_option('api_key');
+            $this->vendor_id = $this->get_option('vendor_id');
+            $this->branch_id = $this->get_option('branch_id');
+            $this->terminal_id = $this->get_option('terminal_id');
+            $orderIdString = '?orderId=' . $order_id;
+            $data = array(
+                'api-key' => $this->api_key,
+                'vendor-id' => $this->vendor_id,
+                'branch-id' => $this->branch_id,
+                'terminal-id' => $this->terminal_id,
+                'email' => $order_billing_email,
+                'customer-name' => $order_billing_first_name . " " . $order_billing_last_name,
+                'amount' => strval($total) ,
+                'description' => $order_id,
+                'date' => $date->format('Y-m-d H:i:s') ,
+                "External-reference" => $order_id,
+                'notification-mode' => '300',
+                'success-url' => site_url() . '/wc-api/payment_success/' . $orderIdString,
+                'error-url' => site_url() . '/wc-api/payment_failure/' . $orderIdString,
 
             );
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, ($this->testmode ? "https://eps-net-uat.sadadbh.com" : "https://eps-net.sadadbh.com") . "/api/v2/web-ven-sdd/epayment/create/");
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-            $results = curl_exec($ch);
-            curl_close($ch);
-            $res = json_decode($results, true);
+
+            $args = array(
+                'body' => json_encode($data) ,
+                'timeout' => '5',
+                'redirection' => '5',
+                'httpversion' => '1.0',
+                'blocking' => true,
+                'headers' => array(
+                    'Content-Type' => 'application/json; charset=utf-8'
+                ) ,
+                'cookies' => array() ,
+                'method' => 'POST',
+                'data_format' => 'body',
+            );
+            $apiurl = ($this->testmode ? "https://eps-net-uat.sadadbh.com" : "https://eps-net.sadadbh.com") . "/api/v2/web-ven-sdd/epayment/create/";
+            $response = wp_remote_post($apiurl, $args);
+			$response_body = json_decode(wp_remote_retrieve_body($response), true);
             return array(
-				'result' => 'success',
-                'redirect' => $res['payment-url'],
-				'messages' => str_replace("BD ","",strval($total)),
-			);
+                'result' => 'success',
+                'redirect' => $response_body['payment-url'],
+                'messages' => str_replace("BD ", "", strval($total)) ,
+            );
             exit();
- 
-	 	}
- 
-		public function payment_success() {
-        // Getting POST data
-        $postData = file_get_contents( 'php://input' );
-        $response  = json_decode( $postData );
-        $orderId = $_GET['orderId'];
-        $order = wc_get_order( $orderId );
-        
-        if ($order) {
-            
-            $order->payment_complete();
-            $order->reduce_order_stock();
-            $order->update_status('processing');
-            global $woocommerce;
-            
-            /*
-	    $mailer = WC()->mailer();
-            $mails = $mailer->get_emails();
-                if ( ! empty( $mails ) )
-                    foreach ( $mails as $mail )
-                        if ( $mail->id == 'customer_completed_order' )
-                            $mail->trigger( $orderId );
-            */
-            header("Location: ".$redirect_url = $this->get_return_url($order));
+
+        }
+
+        public function payment_success()
+        {
+            // Getting POST data
+            $postData = file_get_contents('php://input');
+            $response = json_decode($postData);
+            $orderId = $_GET['orderId'];
+            $order = wc_get_order($orderId);
+
+            if ($order)
+            {
+
+                $order->payment_complete();
+                $order->reduce_order_stock();
+                $order->update_status('processing');
+                global $woocommerce;
+
+                header("Location: " . $redirect_url = $this->get_return_url($order));
             }
         }
-		
-        public function payment_failure() {
-        // Getting POST data
-        $postData = file_get_contents( 'php://input' );
-        $response  = json_decode( $postData );
-        $orderId = $_GET['orderId'];
-        $order = wc_get_order( $orderId );
-        
-			if ($order) {
-            $order->update_status( 'failed');
-            wc_add_notice( __( 'Payment failed.', 'gateway' ), 'error' );
-            wp_safe_redirect( wc_get_page_permalink( 'checkout' ) );
+
+        public function payment_failure()
+        {
+            // Getting POST data
+            $postData = file_get_contents('php://input');
+            $response = json_decode($postData);
+            $orderId = $_GET['orderId'];
+            $order = wc_get_order($orderId);
+
+            if ($order)
+            {
+                $order->update_status('failed');
+                wc_add_notice(__('Payment failed.', 'gateway') , 'error');
+                wp_safe_redirect(wc_get_page_permalink('checkout'));
             }
-		}
- 
-		/*
-		 * In case you need a webhook, like PayPal IPN etc
-		 */
-		public function webhook() {
- 
-		
- 
-	 	}
- 	}
+        }
+
+        /*
+         * In case you need a webhook, like PayPal IPN etc
+        */
+        public function webhook()
+        {
+
+        }
+    }
 }
+
