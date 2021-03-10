cb2d84f4a9e2db38882f95c0745cd255c05b081f
SADAD Electronic Payment System BSC Closed <subscriptions@sadadbahrain.com>
2021-03-10 13:47:47 +0300

    Update index.php
    version fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: SADDED by SADAD Bahrain for card payments
  * Author: Danial Jawaid
  * Author URI: https://www.linkedin.com/in/danial-jawaid-4b527835/
- * Version: 0.2
+ * Version: 0.5
  *
  * License: GNU General Public License v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
